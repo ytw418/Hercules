3bb59eb787ecc6adf01fc3115ed2a96ba52598be
ytw418 <ytw418@gmail.com>
2022-04-20 16:08:17 +0900

    리액트 라우터 버전6 설정

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-import './App.css';
 import React from 'react';
-import BottomTeb from './components/BottomTeb';
-import { createGlobalStyle } from 'styled-components';
-import MainBlock from './components/MainBlock';
-import Header from './components/Header';
-import Home from './components/Home';
-const GlobalStyle = createGlobalStyle`
-  body {
-    background: #e9ecef;
-  }
-`;
+import { Routes, Route, Link } from "react-router-dom";
+import About from './About';
+import Home from './Home';
 
-
-function App() {
+const App = () => {
   return (
-    <>
-    <GlobalStyle />
-    <MainBlock>
-    <Header header_text={"Reactstagram"}></Header>
-    <Home></Home>
-    <BottomTeb></BottomTeb>
-    </MainBlock>
-
-    </>
+    <div className="App">
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
