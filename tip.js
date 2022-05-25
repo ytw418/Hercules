e7b05a22623d6456cf5733d6faccd202c0462769
ytw418 <ytw418@gmail.com>
2022-05-25 16:45:53 +0900

    팁 추가

--- tip.js
+++ tip.js
@@ -21,3 +21,14 @@ clg = console.log(object);
 
 //객체를 키:값 형태로 배열로 반환
 Object.entries(val)
+
+
+
+//파이어베이스 팁
+
+1.   firebase_db.ref(`posts`).orderByChild('date').equalTo(5).once('value').then((snapshot) => {
+         setPosts(snapshot.val());
+      })
+          //date 값이 넘버 5 인 객체 찾기
+
+2. 
