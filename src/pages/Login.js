a865d7f6f92fef22824f8991324173d9abef77ca
ytw418 <ytw418@gmail.com>
2022-06-01 23:15:42 +0900

    seo 최적화를 위한 타이틀 html태그 수정

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -122,7 +122,7 @@ function Login({ setReady, ready }) {
    return (
       <Block>
          <FullLoading isReactLoading={isReactLoading}></FullLoading>
-         <HeaderTitle>breeder</HeaderTitle>
+         <HeaderTitle><title>breeder</title></HeaderTitle>
          <div className='loginBlock'>
             <form onSubmit={onSubmit}>
                <p>이메일</p>
